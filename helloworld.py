9db8d0448c5d70879ac01e565e6fc06bed5aaa74
sk76 <t2rajma33@gmail.com>
2022-02-22 19:34:14 +0545

    fresh python deploy

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Deploying Python Web App Using CI/CD Pipeline'
+    output = b'Fresh ...Deploying Python Web App Using CI/CD Pipeline'
     response_headers = [('Content-type', 'text/plain'),
     ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
